e9302491d801178a967710f712a8da923b0ced8c
Dastan <dastan.namazov.96@gmail.com>
2016-11-28 00:07:19 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@ using namespace cv;
 
 int main(int argc, char *argv[]) {
     Mat image, out;
+    
     image = imread(""/home/tm/lena.jpg"", CV_LOAD_IMAGE_GRAYSCALE);
+    
     if(!image.data)return -1;
+    
     image.covertTo(out, CV_8U)
         
-    for (int y = 0; y < out.cols; y++) {
-        for (int x = 0; x < out.rows; x++) {
-            out.at<uchar>(y, x) /= 2;
-        }
-    }
-        threshold(out, out, 128, 255, THRESH_TRUNC);
-        threshold (out,out,64,128,THRESH_BINARY);
-        threshold(out, out, 32, 64, THRESH_TRUNC);
-        threshold(out, out, 16, 255, THRESH_TRUNC);
+    for (int y = 0; y < out.cols; y++)  for (int x = 0; x < out.rows; x++)  out.at<uchar>(y, x) /= 2;
+
+//    threshold(out, out, 128, 255, THRESH_TRUNC);
+    threshold (out,out,64,128,THRESH_BINARY);
+//    threshold(out, out, 32, 64, THRESH_TRUNC);
+//    threshold(out, out, 16, 255, THRESH_TRUNC);
     
     waitKey(0);
     return 0;
